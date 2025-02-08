dff7f19ee9f460795384a0d478682a3bf8ba6906
Jozef Barčák <jbarcakw@gmail.com>
2025-02-08 18:07:44 +0100

    Delete Architektúra PC directory

--- Architektúra PC/Blok 2/cmake-build-debug/CMakeFiles/edit_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd "/cygdrive/c/Users/jojob/OneDrive/Počítač/Architektúra PC/Blok 2/cmake-build-debug"
-/cygdrive/c/Users/jojob/AppData/Local/JetBrains/CLion2023.3/cygwin_cmake/bin/cmake.exe -E echo No\ interactive\ CMake\ dialog\ available.
